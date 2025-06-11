90696c4978a8c0e86c15eaecd1e4f3d291b792d9
ajmwegman <5818229+ajmwegman@users.noreply.github.com>
2025-06-11 22:28:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php require_once("config.php"); ?>
+<!DOCTYPE html>
+<html lang="nl"><head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title><?php echo $title; ?></title>
+  <meta content="<?php echo $meta_description; ?>" name="description">
+  <meta content="<?php echo $meta_keywords; ?>" name="keywords">
+
+  <!-- Favicons -- >
+  <link href="themes/onepage/assets/img/favicon.png" rel="icon">
+  <link href="themes/onepage/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Dosis:300,400,500,,600,700,700i|Lato:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="<?php echo $site_location; ?><?php echo $theme; ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo $site_location; ?><?php echo $theme; ?>/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?php echo $site_location; ?><?php echo $theme; ?>/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?php echo $site_location; ?><?php echo $theme; ?>/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="<?php echo $site_location; ?><?php echo $theme; ?>/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!--- Template Main CSS File -->
+  <link href="<?php echo $site_location; ?><?php echo $theme; ?>/assets/css/style.css" rel="stylesheet">
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<style>
+	.navbar-collapse { display: block !important; }
+</style>
+</head>
+
+<body>
+<div id="display" class="alert-fixed"></div>
+<?php 
+require_once($theme."/sections/header.php"); 
+    
+require_once($_SERVER['DOCUMENT_ROOT']."/modules/carousel.php");
+    
+        echo $sessid;
+
+require_once($theme."/sections/sections.php");
+require_once($_SERVER['DOCUMENT_ROOT']."/modules/gallery.php");
+require_once($theme."/sections/contact.php");
+require_once($theme."/sections/footer.php");
+?>
